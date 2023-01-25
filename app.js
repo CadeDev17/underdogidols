ca13573c29bc7c6f19246183b621a5fd2703b001
Decryptr22 <decryptr22@gmail.com>
2023-01-25 13:01:26 -0500

    Heroku Config

--- app.js
+++ app.js
@@ -163,7 +163,7 @@ passport.deserializeUser(function(id, done) {
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
-    app.listen(3000);
+    app.listen(process.env.PORT || 3000);
   })
   .catch(err => {
     console.log(err);

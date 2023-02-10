9570568b0c658c790f45e4395ef54afeb30f95a4
Decryptr22 <decryptr22@gmail.com>
2023-02-09 19:53:09 -0500

    Fixed Voting

--- middleware/isFan.js
+++ middleware/isFan.js
@@ -1,6 +1,8 @@
 module.exports = (req, res, next) => {
     if (req.session.passport.user[0].userType !== 'Fan') {
         return res.redirect('/signin');
+    } else if (req.session.user[0].userType !== 'Fan') {
+        return res.redirect('/signin');
     }
     next();
 }

4c866d775374cd71446ea89230c71c761a764434
Decryptr22 <decryptr22@gmail.com>
2023-02-10 10:14:51 -0500

    404 & 500

--- middleware/isFan.js
+++ middleware/isFan.js
@@ -2,10 +2,6 @@ module.exports = (req, res, next) => {
     console.log(req)
     if (req.session.passport.user[0].userType !== 'Fan') {
         return res.redirect('/signin');
-    } else if (req.session.user[0].userType !== 'Fan') {
-        return res.redirect('/signin');
-    } else {
-        res.redirect('/signin');
     }
     next();
 }

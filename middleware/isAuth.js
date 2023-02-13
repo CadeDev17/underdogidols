ae51eb5494454d0bf3a7be2e4625270779bbd805
Decryptr22 <decryptr22@gmail.com>
2023-02-13 10:31:31 -0500

    S1

--- middleware/isAuth.js
+++ middleware/isAuth.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-    if (!req.session.isLoggedIn || req.session.passport.user[0].userType !== 'Fan') {
+    if (!req.session.isLoggedIn || req.session.passport.user[0].userType === 'Fan') {
         return res.redirect('/signin');
     }
     next();
